39ae3a7fc02acae3bb25ac4856abf28ffaa13941
Ethan Vander Horn <ethan.vanderh123@gmail.com>
2021-02-24 13:52:09 -0800

    updating the item handling for the new prefab.

--- testing/test_assets/FileExplorer.py
+++ testing/test_assets/FileExplorer.py
@@ -10,6 +10,7 @@ from nanome.util.file import FileError
 
 test_assets = os.getcwd() + ("/testing/test_assets")
 
+
 class Icon():
     up = "test_assets/PluginIcons/Up.png"
     folder = "test_assets/PluginIcons/Folder.png"
@@ -17,6 +18,7 @@ class Icon():
     pdf = "test_assets/PluginIcons/PDF.png"
     structure = "test_assets/PluginIcons/Structures.png"
     workspace = "test_assets/PluginIcons/Workspace.png"
+
 
 class FileExplorer():
     def __init__(self):
@@ -38,7 +40,7 @@ class FileExplorer():
         self.selected_button = None
         self.temp_dir = tempfile.mkdtemp()
 
-    def on_up_pressed (self):
+    def on_up_pressed(self):
         pass
 
     def on_directory_pressed(self, entry):
@@ -47,17 +49,20 @@ class FileExplorer():
     def on_select_pressed(self):
         pass
 
-    def on_quick_access(self, name):
+    def on_quick_access(self, button):
         pass
 
-    def set_quick_access_list (self, names):
+    def set_quick_access_list(self, names):
         self.file_source_node.clear_children()
         for name in names:
             new_node = self.quick_access_prefab.clone()
-            new_node.get_content().name = name
+            button = new_node.get_content()
+            button.name = name
+            button.text.value.set_all(name)
+            button.register_pressed_callback(self.on_quick_access)
             self.file_source_node.add_child(new_node)
 
-    def set_working_directory (self, error, path):
+    def set_working_directory(self, error, path):
         self.path_text.text_value = path
 
     def set_children(self, error, files):
@@ -71,46 +76,75 @@ class FileExplorer():
                 self.grid.items.append(item)
 
     def __entry_pressed(self, button):
-        if button.entry.is_directory:
-            self.on_directory_pressed(button.entry)
-            return
         to_update = []
-        button.selected = True
-        if self.selected_button is not None:
-            self.selected_button.selected = False
-            to_update.append(self.selected_button)
+        # deselecting a node
         if self.selected_button == button:
             self.selected_button = None
+            button.selected = False
+            to_update.append(button)
+            self.select_button.unusable = True
+            to_update.append(self.select_button)
+        # selecting a node
         else:
+            #deselecting the old node
+            if self.selected_button is not None:
+                self.selected_button.selected = False
+                to_update.append(self.selected_button)
+            else:
+                self.select_button.unusable = False
+                to_update.append(self.select_button)
+            button.selected = True
+            to_update.append(button)
             self.selected_button = button
-            to_update.append(self.selected_button)
+        # self.update_content(to_update)
 
-        self.select_button.unusable = self.selected_button == None
-        to_update.append(self.select_button)
-        #self.update_content(to_update)
+    def __directory_pressed(self, button):
+        self.on_directory_pressed(button.entry)
+
+    def __get_prefab_parts(self, prefab):
+        selection_button = prefab.find_node("Selection", True).get_content()
+        icon = prefab.find_node("Icon", True).get_content()
+        text = prefab.find_node("Name", True).get_content()
+
+        favorite_node = prefab.find_node("Favorite", True)
+        favorite_button = favorite_node.get_content()
+
+        date_and_size_node = prefab.find_node("DateAndSize", True).get_content()
+        date = prefab.find_node("Date", True).get_content()
+        size = prefab.find_node("Size", True).get_content()
+        return selection_button, icon, text, favorite_node, favorite_button, date_and_size_node, date, size
 
     def __create_file_rep(self, entry):
         extension = os.path.splitext(entry.name)[1].lower()
-        icon = self.__get_icon(extension)
-        if (icon == None):
+        image = self.__get_icon(extension)
+        if (image == None):
             return None
 
         item = self.item_prefab.clone()
-        button = item.find_node("Button", True).get_content()
-        button.text.value.set_all(entry.name)
-        button.register_pressed_callback(self.__entry_pressed)
-        button.entry = entry
-        button.text.value.set_all(self.__path_leaf(entry.name))
-        button.text.size = .3
-        button.text.ellipsis = True
-        button.icon.value.set_all(icon)
+        selection_button, icon, text, favorite_node, favorite_button, date_and_size_node, date, size = self.__get_prefab_parts(item)
+
+        selection_button.entry = entry
+
+        icon.icon.value.set_all(image)
+        text.text_value.set_all(self.__path_leaf(entry.name))
+
+        if entry.is_directory:
+            selection_button.register_pressed_callback(self.__directory_pressed)
+            # favorite_node.enabled(True) #Disabling favorite behaviour for now
+            date_and_size_node.enabled(False)
+        else:
+            selection_button.register_pressed_callback(self.__entry_pressed)
+            # favorite_node.enabled(False)
+            date_and_size_node.enabled(True)
+            date.text_value.set_all(entry.date_modified)
+            size.text_value.set_all(entry.size)
         return item
 
     def __path_leaf(self, path):
         head, tail = ntpath.split(path)
         return tail or ntpath.basename(head)
 
-    def __get_icon (self, extension):
+    def __get_icon(self, extension):
         if extension == "":
             return Icon.folder
         elif extension == ".pdf":
@@ -119,7 +153,7 @@ class FileExplorer():
             return Icon.image
         elif extension == ".nanome":
             return Icon.workspace
-#region structure formats
+# region structure formats
         elif extension == ".pdb" or extension == ".pdb1" or extension == ".pdb2" or extension == ".pdb3" or extension == ".pdb4" or extension == ".pdb5":
             return Icon.structure
         elif extension == ".sd" or extension == ".sdf" or extension == ".mol" or extension == ".mol2":
@@ -132,8 +166,8 @@ class FileExplorer():
             return Icon.structure
         elif extension == ".moe" or extension == ".mae" or extension == ".pse":
             return Icon.structure
-#endregion
-#region misc formats
+# endregion
+# region misc formats
         elif extension == ".dcd" or extension == ".xtc" or extension == ".trr" or extension == ".psf":
             return Icon.structure
         elif extension == ".ccp4" or extension == ".dsn6":
@@ -142,4 +176,4 @@ class FileExplorer():
             return Icon.structure
         else:
             return None
-#endregion
+# endregion
